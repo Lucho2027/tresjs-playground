ae15c6982ad6d502e7ffb6853ac2acf192225883
Lucho2027 <luis01.ro@gmail.com>
2024-06-06 06:47:40 -0500

    setting up 3d render canvas for playground

---
+++ app.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+// const spaceManRef = ref();
+// const {
+//   scene: scenes,
+//   animations,
+//   scene,
+// } = await useGLTF("/spaceManFalling.glb");
+
+// onMounted(async () => {
+//   if (!spaceManRef.value) return;
+//   const { actions } = await useAnimations(animations, spaceManRef);
+//   actions["Idle"].play();
+// });
+</script>
+<template>
+  <TresCanvas window-size clear-color="black" :alpha="true">
+    <TresPerspectiveCamera :position="[1, 1, 5]" :look-at="[0, 0, 0]" />
+
+    <OrbitControls />
+    <!-- <MouseParallax
+      :factor="1"
+      :ease="3"
+    /> -->
+    <Precipitation
+      :speed="0.05"
+      :count="5"
+      :randomness="0"
+      color="#4285f4"
+      :size="0.02"
+    />
+    <Precipitation
+      :speed="0.015"
+      :count="5"
+      :randomness="0"
+      color="#34A853"
+      :size="0.02"
+    />
+    <Precipitation
+      :speed="0.015"
+      :count="5"
+      :randomness="0"
+      color="#FBBC05"
+      :size="0.02"
+    />
+    <Precipitation
+      :speed="0.015"
+      :count="5"
+      :randomness="0"
+      color="#EA4335"
+      :size="0.02"
+    />
+    <!-- <TresMesh > -->
+    <Suspense>
+      <Astronaut />
+    </Suspense>
+    <!-- </TresMesh> -->
+  </TresCanvas>
+</template>
---
+++ components/Astronaut.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const spaceManRef = ref();
+const { scene, animations } = await useGLTF("/spaceManFalling.glb");
+
+onMounted(async () => {
+  console.log("asdasd", spaceManRef.value);
+  if (!spaceManRef.value) return;
+  const { actions } = await useAnimations(animations, spaceManRef);
+  console.log(actions);
+  actions["Idle"].play();
+});
+// const { onLoop } = useRenderLoop();
+// onLoop(async () => {
+//   if (!spaceManRef.value) return;
+//   const { actions } = await useAnimations(animations, spaceManRef);
+
+//   actions["Idle"].play();
+// });
+</script>
+
+<template>
+  <primitive :object="scene" ref="spaceManRef" />
+</template>
+
+<style scoped></style>
